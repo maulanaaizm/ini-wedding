26d8fbd154e61a281e121f12014ae08a18a28d90
maulanaaizm <maulanaaizm4@gmail.com>
2023-10-01 15:13:59 +0700

    mengubah tampilan home

--- script.js
+++ script.js
@@ -6,26 +6,25 @@ const audioIcon = document.querySelector(".audio-icon-wrapper i");
 const navbar = document.querySelector(".navbar");
 let isPlaying = false;
 
-function disableScroll() {
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+// function disableScroll() {
+//   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
-  window.onscroll = function () {
-    window.scrollTo(scrollTop, scrollLeft);
-  };
+//   window.onscroll = function () {
+//     window.scrollTo(scrollTop, scrollLeft);
+//   };
 
-  rootElement.style.scrollBehavior = "auto";
-}
+//   rootElement.style.scrollBehavior = "auto";
+// }
 
-function enableScroll() {
-  window.onscroll = function () {};
-  rootElement.style.scrollBehavior = "smooth";
-  navbar.style.display = "block";
-  // localStorage.setItem("opened", "true");
+// function enableScroll() {
+//   window.onscroll = function () {};
+//   rootElement.style.scrollBehavior = "smooth";
+//   navbar.style.display = "block";
+//   // localStorage.setItem("opened", "true");
 
-  playAudio();
-  navbar();
-}
+//   navbar();
+// }
 
 function playAudio() {
   // song.volume = 0.1;
@@ -48,4 +47,15 @@ audioIconWrapper.onclick = function () {
   isPlaying = !isPlaying;
 };
 
-disableScroll();
+// disableScroll();
+
+const main = document.querySelector("#main");
+const hero = document.querySelector("#hero");
+const buka = document.querySelector("#buka");
+
+buka.addEventListener("click", function () {
+  main.style.display = "block";
+  hero.style.display = "none";
+  navbar.style.display = "block";
+  playAudio();
+});
